8707075c5e338eb54ff08a5a2631b4edb71d0a61
Phil <higgins.phillip@gmail.com>
2016-06-16 19:02:24 +0100

    Stopping for dinner

--- LoadData/Loaders/SQLDBLoader.cs
+++ LoadData/Loaders/SQLDBLoader.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Data.Repositories.Models;
+using Data.Repositories;
+using LoadData.Constants;
+using System.IO;
 
 namespace LoadData.Loaders
 {
@@ -12,5 +15,44 @@ namespace LoadData.Loaders
     /// </summary>
     internal class SQLDBLoader : LoaderBase
     {
+        private List<string> _fileNames = null;
+        NodeRepository _nodeRepo = null;
+
+        public SQLDBLoader() 
+            : base()
+        {
+            _nodeRepo = new NodeRepository();
+        }
+
+        public SQLDBLoader(string inputPath)
+            : base(inputPath)
+        {
+            _fileNames = new List<string>();
+            _nodeRepo = new NodeRepository();
+        }
+
+        /// <summary>
+        /// Populate the list of files in teh path specified
+        /// </summary>
+        /// <returns>Success if validation passes</returns>
+        public int GetFiles()
+        {
+            int validationResult = 0;
+
+            validationResult = Validate();
+
+            if (validationResult == LoadConstants.Success)
+            {
+                _fileNames = Directory.GetFiles(InputPath).ToList<string>();
+            }
+
+            return validationResult;
+        }
+
+
+        //Insert Data
+
+        //Load Files
+
     }
 }
